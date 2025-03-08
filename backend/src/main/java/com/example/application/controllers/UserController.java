50110424e8f6c16405627e0602471653ed6824ab
Ahtesham Ansari <ahteshamansari@192.168.1.4>
2025-03-08 16:46:01 +0530

    Final Solution

--- backend/src/main/java/com/example/application/controllers/UserController.java
+++ backend/src/main/java/com/example/application/controllers/UserController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserRepository userRepository;
